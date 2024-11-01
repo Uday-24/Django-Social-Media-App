24479295aeb4b568327ebcc2cd59cb30f7653789
Uday-24 <chotaliyauday24@gmail.com>
2024-11-01 16:17:46 +0530

    Login and logout added

--- socialapp/users/views.py
+++ socialapp/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+def user_login(request):
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+
+    form = LoginForm()
+    return render(request, 'users/login.html', {'form' : form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'users/index.html')
