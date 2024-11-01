a4ab9cc3dac16f266c63784df682a2f36a59abbb
Uday-24 <chotaliyauday24@gmail.com>
2024-11-01 15:19:50 +0530

    Social media project start

--- socialapp/socialapp/settings.py
+++ socialapp/socialapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=g5723&k)t6*@$lgdjf!y=rhc4@#87tj!ld6^6sql^ia0p*%v^'
+SECRET_KEY = 'django-insecure-4itpi_y_+^cjxiiw1^0kx^($mqobk26sm!2!beqeek^!il24p6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
 ]
 
 MIDDLEWARE = [
--- socialapp/users/views.py
+++ socialapp/users/views.py
@@ -1,29 +1,3 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .forms import LoginForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
 # Create your views here.
-def user_login(request):
-
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-            
-    form = LoginForm()
-    return render(request, 'users/login.html', {'form' : form})
-
-@login_required
-def index(request):
-    return render(request, 'users/index.html')
-
-def logout_page(request):
-    logout(request)
-    return redirect('login')
